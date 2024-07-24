7b0889d7fd19565ead9b19cc432013bb46365000
Marek655 <89846384+Marek655@users.noreply.github.com>
2024-07-24 09:44:07 +0200

    Add files via upload
    Modified code where I found some bugs. Translated to Slovak language.

---
+++ lang.ino
@@ -0,0 +1,23 @@
+  char* menu1[]={"NAJDI&ZNESKODNI","SABOTAZ","DOMINANCIA"      };
+  char* menu2[]={"Game Config","Sound Config", "Mosfet Test", "Auto Test"      };   
+  char* GAME_TIME="HRACI CAS:";
+  char* BOMB_TIME="CASOVAC BOMBY:";
+  char* ZERO_MINUTES="00 MINUT";
+  char* ARM_TIME="CAS PLANTOVANIA:";
+  char* ZERO_SECS="00 SEKUND";
+  char* ENABLE_SOUND="POVOLIT ZVUKY ?";
+  char* YES_OR_NOT="A : ANO B : NIE";
+  char* ENABLE_MOSFET="POVOLIT SIRENU ?";
+  char* ENABLE_CODE="NASTAVIT PIN ?";
+  char* GAME_TIME_TOP="HRACI CAS";
+  char* ARMING_BOMB = "PLANTOVANIE";
+  char* ENTER_CODE = "ZADAJ PIN";
+  char* CODE_ERROR = "NESPRAVNY PIN !";
+  char* BOMB_ARMED = "BOMBA PLANTNUTA";
+  char* DETONATION_IN = "VYBUCH ZA:";
+  char* DISARMING = "ZNESKODNOVANIE" ;
+  char* DISARM = "ZNESKODNENA !";
+  char* GAME_OVER = "KONIEC HRY !";
+  char* DEFENDERS_WIN = "POLICIA VYHRALA";
+                   
+  
